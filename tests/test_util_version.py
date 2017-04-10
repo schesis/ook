9294ca0c83fcd4a84a995de1b82b6b5a7652c36b
Zero Piraeus <z@etiol.net>
2017-04-10 20:08:43 -0300

    Update test parameterization.

--- tests/test_util_version.py
+++ tests/test_util_version.py
@@ -14,18 +14,22 @@ from ook.util import Version
 from tests import VERSION_ARGS
 
 
-def check_repr(version):
+@pytest.mark.parametrize('version', map(Version, VERSION_ARGS))
+def test_repr(version):
     """`Version` instance has a sensible `repr` value"""
     assert repr(version) == "Version(%r)" % str(version)
 
 
-def test_repr():
-    """`Version` instances have sensible `repr` values"""
+def params_mmm():
+    """yield `Version` instances and `major`|`minor`|`micro` attributes"""
+    attrs = ("major", "minor", "micro")
     for version in map(Version, VERSION_ARGS):
-        yield check_repr, version
+        for index, attr in enumerate(getattr(version, a) for a in attrs):
+            yield version, index, attr
 
 
-def check_mmm(version, index, attr):
+@pytest.mark.parametrize('version,index,attr', params_mmm())
+def test_mmm(version, index, attr):
     """`Version` instance has sensible `major`|`minor`|`micro` attributes"""
     # pylint: disable=redefined-variable-type
     try:
@@ -37,38 +41,32 @@ def check_mmm(version, index, attr):
     assert attr == expected_value
 
 
-def test_mmm():
-    """`Version` instances have sensible `major`|`minor`|`micro` attributes"""
-    attrs = ("major", "minor", "micro")
-    for version in map(Version, VERSION_ARGS):
-        for index, attr in enumerate(getattr(version, a) for a in attrs):
-            yield check_mmm, version, index, attr
-
-
-def check_compare(oper, ver1, ver2):
-    """``Version` instances compare as expected"""
-    assert oper(ver1, ver2) == oper(ver1.truncate(ver2), ver2.truncate(ver1))
-
-
-def test_compare():
-    """``Version` instances compare as expected"""
+def params_compare():
+    """yield `Version` comparisons"""
     for ver1 in map(Version, VERSION_ARGS):
         for ver2 in map(Version, VERSION_ARGS):
             for oper in (eq, ge, gt, le, lt, ne):
-                yield check_compare, oper, ver1, ver2
+                yield oper, ver1, ver2
 
 
-def check_compare_unorderable(oper, version, nonversion):
-    """Comparing `Version` instance against non-`Version` fails"""
-    with pytest.raises(TypeError):
-        oper(version, nonversion)
+@pytest.mark.parametrize('oper,ver1,ver2', params_compare())
+def test_compare(oper, ver1, ver2):
+    """`Version` instances compare as expected"""
+    assert oper(ver1, ver2) == oper(ver1.truncate(ver2), ver2.truncate(ver1))
 
 
-def test_compare_unorderable():
-    """Comparing `Version` instances against non-`Version`s fails"""
+def params_cmp_unorderable():
+    """yield `Version`, non-`Version` comparisons"""
     for nonversion in VERSION_ARGS:
         if isinstance(nonversion, Version):
             continue
         for version in map(Version, VERSION_ARGS):
             for oper in (ge, gt, le, lt):
-                yield check_compare_unorderable, oper, version, nonversion
+                yield oper, version, nonversion
+
+
+@pytest.mark.parametrize('oper,version,nonversion', params_cmp_unorderable())
+def test_cmp_unorderable(oper, version, nonversion):
+    """Comparing `Version` instance against non-`Version` fails"""
+    with pytest.raises(TypeError):
+        oper(version, nonversion)
